b42c80a32f2505e081f9197a7109e61645d38d20
ryannicholas <ryannicholas.vieira@gmail.com>
2023-01-02 16:58:35 -0300

    first commit

---
+++ backend/src/server.ts
@@ -0,0 +1,8 @@
+import app from "./app";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT: number = Number(process.env.PORT);
+
+app.listen(PORT, () => console.log(`Server is up on port ${PORT}!`));
