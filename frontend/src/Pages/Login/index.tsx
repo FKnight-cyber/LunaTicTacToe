055e0e0b3ceee10f79a6a61ddf13df1d0ab12a93
ryannicholas <ryannicholas.vieira@gmail.com>
2023-01-03 10:33:12 -0300

    feat: :lipstick: add login layout

--- frontend/src/Pages/Login/index.tsx
+++ frontend/src/Pages/Login/index.tsx
@@ -1,9 +1,27 @@
-import Container from "./style"
+import Container from "./style";
+import loginImg from "../../assets/LunaLogin.png";
 
 export default function Login() {
   return(
     <Container>
-      <h1>login</h1>
+      <img src={loginImg} alt="" />
+      <div className="pageInfo">
+        <form>
+          <input 
+            type="text" 
+            value=""
+            placeholder="Username"
+          />
+          <input 
+            type="password" 
+            value=""
+            placeholder="Password"
+          />
+          <button type="submit">Login</button>
+        </form>
+        <h3>Don't have an account?</h3>
+        <h3>Register now</h3>
+      </div>
     </Container>
   )
 };
--- frontend/src/Pages/Login/style.tsx
+++ frontend/src/Pages/Login/style.tsx
@@ -1,9 +1,72 @@
 import styled from "styled-components";
 
 const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   width: 100%;
   height: 100vh;
   background-color: #ECBBBE;
+
+  .pageInfo {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  img{
+    width: 300px;
+    height: 300px;
+    object-fit: cover;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    height: 150px;
+    
+    input {
+      width: 280px;
+      height: 42px;
+    }
+
+    button {
+      width: 200px;
+      background-color: #4385F2;
+      color: #ffffff;
+      height: 30px;
+      margin-bottom: 10px;
+    }
+  }
+
+  @media only screen and (min-width: 940px)  {
+    flex-direction: row;
+    padding-right: 40px;
+
+    img {
+      height: 500px;
+      width: 50%;
+    }
+  }
+
+  @media only screen and (min-width: 1160px)  {
+    position: relative;
+    img {
+      height: 600px;
+      width: 50%;
+   
+    }
+
+    .pageInfo {
+      position: absolute;
+      bottom: auto;
+      right: 26%;
+    }
+  }
 `
 
 export default Container;
